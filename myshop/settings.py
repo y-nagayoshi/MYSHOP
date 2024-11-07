7c7baeaf122a1e0ed17001a26046a01b3eb20bef
y-nagayoshi <wakasagi135@gmail.com>
2024-11-07 23:40:59 +0900

    クーポン機能実装

--- myshop/settings.py
+++ myshop/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'orders.apps.OrdersConfig',
     'payment.apps.PaymentConfig',
     'shop.apps.ShopConfig',
+    'coupons.apps.CouponsConfig',
 ]
 
 MIDDLEWARE = [
--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.conf import settings
+from decimal import Decimal
+from django.core.validators import MaxValueValidator, MinValueValidator
+from coupons.models import Coupon
 
 # Create your models here.
 class Order(models.Model):
@@ -13,6 +16,17 @@ class Order(models.Model):
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
     stripe_id = models.CharField(max_length=250, blank=True)
+    coupon = models.ForeignKey(
+        Coupon,
+        related_name='orders',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+    discount = models.IntegerField(
+        default=0,
+        validators = [MinValueValidator(0), MaxValueValidator(100)]
+    )
     
     class Meta:
         ordering = ['-created']
@@ -24,7 +38,8 @@ class Order(models.Model):
         return f'Order {self.id}'
     
     def get_total_cost(self):
-        return sum(item.get_cost() for item in self.items.all())
+        total_cost = self.get_total_cost_before_discount()
+        return total_cost - self.get_discount()
     
     def get_stripe_url(self):
         if not self.stripe_id:
@@ -34,6 +49,15 @@ class Order(models.Model):
         else:
             path = '/'
         return f'https://dashboard.stripe.com{path}payments/{self.stripe_id}'
+    
+    def get_total_cost_before_discount(self):
+        return sum(item.get_cost() for item in self.items.all())
+    
+    def get_discount(self):
+        total_cost = self.get_total_cost_before_discount()
+        if self.discount:
+            return total_cost * (self.discount / Decimal(100))
+        return Decimal(0)
     
 class OrderItem(models.Model):
     order = models.ForeignKey(
